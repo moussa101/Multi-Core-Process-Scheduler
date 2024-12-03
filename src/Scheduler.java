5fac0870e8d1577cf1cced5a64fcc916c58d5016
Moussa <abdelrhanmoussa21@gmail.com>
2024-12-03 17:26:17 +0200

    discovered an issue regarding algorithm type missing in Scheduler Class

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -3,30 +3,51 @@ import java.util.*;
 class Scheduler {
     private Queue<Process> processQueue;
     private static final int quantum = 2;
+    private String Algorithm; //set Algorithm type e.g., Round Robin, SJF
 
+    public Scheduler(List<Process> processes, String Algorithm ) {
+        this.processQueue = new LinkedList<>(processes); //?Process list probably belongs in master-core?
+        this.Algorithm = Algorithm;
+    }
     public Scheduler(List<Process> processes) {
         this.processQueue = new LinkedList<>(processes); //?Process list probably belongs in master-core?
+    }
+
+    public void setAlgorithm(String Algorithm){
+        if (Algorithm.equals("Round Robin")||Algorithm.equals("SJF")){
+            this.Algorithm = Algorithm;
+        }
     }
 
     public void addProcess(Process Process){
         processQueue.offer(Process);
     }
 
-    public Process scheduleNext() {
+    public Process scheduleNext()  { // (issue)
         if (processQueue.isEmpty()) {
-            return null; // No processes left to schedule
+            System.out.println("All Process has been finished");
+            return null;// No processes left to schedule
         }
+
         Process currentProcess = processQueue.poll();
-        for (int i = 0; i < quantum; i++) {
+        if (Algorithm.equals("Round Robin")) {
+            for (int i = 0; i < quantum; i++) {
+                if (!currentProcess.isComplete()) {
+                    String instruction = currentProcess.getNextInstruction();
+                    System.out.println("Executing instruction: " + instruction + " from Process ID: " + currentProcess.getProcessID());
+                } else {
+                    break;
+                }
+            }
             if (!currentProcess.isComplete()) {
-                String instruction = currentProcess.getNextInstruction();
-                System.out.println("Executing instruction: " + instruction + " from Process ID: " + currentProcess.getProcessID());
-            } else {
-                break;
+                processQueue.offer(currentProcess); // Re-add the process if it's not complete
             }
         }
-        if (!currentProcess.isComplete()) {
-            processQueue.offer(currentProcess); // Re-add the process if it's not complete
+        if (Algorithm.equals("SJF")){
+            // !!!!!!!-------------------- Write the SJF Algorithm -------------------------!!!!!!
+        }
+        else {
+            System.out.println("The Algorithm for this Scheduler has not been yet set");
         }
         return currentProcess;
     }
